c8d7763a000a286d85c956bc5b32f3a3c8cdc318
Pascaline M <pascaline.martres@gmail.com>
2020-12-21 17:24:20 -0800

    edit report

--- models/database.py
+++ models/database.py
@@ -56,10 +56,8 @@ class Data:
         """ Update tournament's rounds and matches in the database."""
         db = TinyDB("db.json")
         tournament_table = db.table("TOURNAMENTS")
-        players_by_tournament = db.table("PLAYERS")
         list_ser_rounds = []
         list_ser_matches = []
-        list_players = players_by_tournament.all()
         for round in rounds:
             ser_round = Round.serialized_round(round)
             list_ser_rounds.append(ser_round)
@@ -80,7 +78,6 @@ class Data:
             {
                 "rounds": list_ser_rounds,
                 "matches": list_ser_matches,
-                "players": list_players,
             },
             doc_ids=[id_tournament],
         )
@@ -91,8 +88,8 @@ class Data:
         actors_table = db.table("ACTORS")
         players_by_tournament = db.table("PLAYERS")
         user = Query()
-        first_name = input("First name ? ")
-        last_name = input("Last name ? ")
+        first_name = input("First name ? ").capitalize()
+        last_name = input("Last name ? ").capitalize()
         new_rank = int(input("New rank ? "))
         actors_table.update(
             {"rank": new_rank},
@@ -109,8 +106,8 @@ class Data:
         actors_table = db.table("ACTORS")
         players_by_tournament = db.table("PLAYERS")
         user = Query()
-        first_name = input("First name ?")
-        last_name = input("Last name ?")
+        first_name = input("First name ?").capitalize()
+        last_name = input("Last name ?").capitalize()
         new_score = int(input("New score ? "))
 
         players_by_tournament.update(
@@ -122,40 +119,42 @@ class Data:
         """Sort all actors by alphabetic order or by rank."""
         db = TinyDB("db.json")
         actors_table = db.table("ACTORS")
-        sorted_actors = int(input("Sorted by Last Name (1) or by Rank (2) ? "))
         all_data_actors = actors_table.all()
         list_actors = []
-        for nb in range(len(all_data_actors)):
+        for actor in range(len(all_data_actors)):
             data_actors = (
-                all_data_actors[nb].get("last name"),
-                all_data_actors[nb].get("first name"),
-                all_data_actors[nb].get("birth date"),
-                all_data_actors[nb].get("gender"),
-                all_data_actors[nb].get("rank"),
-                all_data_actors[nb].get("score"),
+                all_data_actors[actor].get("first name"),
+                all_data_actors[actor].get("last name"),
+                all_data_actors[actor].get("birth date"),
+                all_data_actors[actor].get("gender"),
+                all_data_actors[actor].get("rank"),
+                all_data_actors[actor].get("score"),
             )
             list_actors.append(data_actors)
+        sorted_actors = int(input("Sorted by Last Name (1) or by Rank (2) ? "))
         if sorted_actors == "1":
-            sorted_list = sorted(list_actors, key=lambda colonnes: colonnes[1])
+            sorted_list = sorted(list_actors, key=lambda colonnes: colonnes[2])
         else:
-            sorted_list = sorted(list_actors, key=lambda colonnes: colonnes[5])
+            sorted_list = sorted(
+                list_actors, key=lambda colonnes: colonnes[5], reverse=True
+            )
         print(sorted_list)
 
     def sorted_players():
-        """Sort all players by alphabetic order or by rank."""
+        """Sort all players by alphabetic order or by rank for the current tournamen."""
         db = TinyDB("db.json")
         players_by_tournament = db.table("PLAYERS")
         sorted_players = int(input("Sorted by Last Name (1) or by Rank (2) ? "))
         all_data_players = players_by_tournament.all()
         list_players = []
-        for nb in range(len(all_data_players)):
+        for player in range(len(all_data_players)):
             data_players = (
-                all_data_players[nb].get("last name"),
-                all_data_players[nb].get("first name"),
-                all_data_players[nb].get("birth date"),
-                all_data_players[nb].get("gender"),
-                all_data_players[nb].get("rank"),
-                all_data_players[nb].get("score"),
+                all_data_players[player].get("last name"),
+                all_data_players[player].get("first name"),
+                all_data_players[player].get("birth date"),
+                all_data_players[player].get("gender"),
+                all_data_players[player].get("rank"),
+                all_data_players[player].get("score"),
             )
             list_players.append(data_players)
         if sorted_players == "1":
@@ -171,15 +170,15 @@ class Data:
         tournament_table = db.table("TOURNAMENTS")
         all_data_tournaments = tournament_table.all()
         list_tournaments = []
-        for nb in range(len(all_data_tournaments)):
+        for tournament in range(len(all_data_tournaments)):
             data_tournaments = (
-                all_data_tournaments[nb].get("name"),
-                all_data_tournaments[nb].get("location"),
-                all_data_tournaments[nb].get("date"),
-                all_data_tournaments[nb].get("mode"),
-                all_data_tournaments[nb].get("rounds"),
-                all_data_tournaments[nb].get("description"),
-                all_data_tournaments[nb].get("players"),
+                all_data_tournaments[tournament].get("name"),
+                all_data_tournaments[tournament].get("location"),
+                all_data_tournaments[tournament].get("date"),
+                all_data_tournaments[tournament].get("mode"),
+                all_data_tournaments[tournament].get("rounds"),
+                all_data_tournaments[tournament].get("description"),
+                all_data_tournaments[tournament].get("players"),
             )
             list_tournaments.append(data_tournaments)
         print(list_tournaments)
@@ -191,7 +190,6 @@ class Data:
         user = Query()
         date = input("Tournament's date ? (format : DD/MM/YYYY)")
         choice = tournament_table.search(user["date"] == date)
-        print(choice)
         request_tournament = int(input("Display ROUNDS (1) or MATCHES (2) ? "))
         if request_tournament == 1:
             print(choice[0].get("rounds"))
@@ -205,13 +203,23 @@ class Data:
         user = Query()
         date = input("Tournament's date ? (format : DD/MM/YYYY) ")
         choice = tournament_table.search(user["date"] == date)
-        print(choice)
+        all_data_players = choice[0].get("players")
+        list_players = []
+        for player in range(len(all_data_players)):
+            data_players = (
+                all_data_players[player].get("first name"),
+                all_data_players[player].get("last name"),
+                all_data_players[player].get("birth date"),
+                all_data_players[player].get("gender"),
+                all_data_players[player].get("rank"),
+                all_data_players[player].get("score"),
+            )
+            list_players.append(data_players)
         sorted_players = int(input("Sorted by Last Name (1) or by Rank (2) ? "))
         if sorted_players == 1:
-            list_players = choice[0].get("players")
-            sorted_list = sorted(list_players, key=lambda colonnes: colonnes[0])
+            sorted_list = sorted(list_players, key=lambda colonnes: colonnes[2])
         else:
-            choice[0].get("players")
-            list_players = choice[0].get("players")
-            sorted_list = sorted(list_players, key=lambda colonnes: colonnes[1])
+            sorted_list = sorted(
+                list_players, key=lambda colonnes: colonnes[5], reverse=True
+            )
         print(sorted_list)
