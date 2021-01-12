56925ee78bd830a760f2236cdf873a92e64e4b8f
Pascaline M <pascaline.martres@gmail.com>
2021-01-11 16:05:10 -0800

    classmethod, differents choix + explicites

--- controllers/menu.py
+++ controllers/menu.py
@@ -10,7 +10,8 @@ from view.report import DisplayReport
 class MenuController:
     """Class controls all the different menus in the program."""
 
-    def create_player():
+    @classmethod
+    def create_player(cls):
         """Create a new player."""
         first_name = NewPlayer.player_first_name()
         last_name = NewPlayer.player_last_name()
@@ -20,58 +21,81 @@ class MenuController:
         score = NewPlayer.player_score()
         return Player(first_name, last_name, birth_date, gender, rank, score)
 
-    def create_tournament(players):
-        """Create a new tournament. """
+    @classmethod
+    def create_tournament(cls, players):
+        """Create a new tournament."""
         name = NewTournament.tournament_name()
         location = NewTournament.tournament_location()
         mode = NewTournament.tournament_mode()
         description = NewTournament.tournament_description()
         return Tournament(name, location, mode, description, players)
 
-    def display_reports(tournament_table, actors_table, user):
-        """Display the reports' menu. """
+    @classmethod
+    def display_reports(cls, tournament_table, actors_table, user):
+        """Display the reports' menu."""
         report = DisplayReport.menu_report()
-        if report == 1:
+        reports = {
+            1: "sorted_actors_report",
+            2: "tournaments_report",
+            3: "rounds_report",
+            4: "players_report",
+        }
+        if reports[report] == "sorted_actors_report":
             display_report = Data.sorted_actors(actors_table)
-            DisplayReport.report_1(display_report)
-        elif report == 2:
+            DisplayReport.report_actors(display_report)
+        elif reports[report] == "tournaments_report":
             display_report = tournament_table.all()
-            DisplayReport.report_2(display_report)
-        elif report == 3:
+            DisplayReport.report_tournaments(display_report)
+        elif reports[report] == "rounds_report":
             display_report = Data.request_tournament(tournament_table, user)
-            DisplayReport.report_3(display_report)
-        elif report == 4:
+            DisplayReport.report_rounds(display_report)
+        elif reports[report] == "players_report":
             display_report = Data.request_players(tournament_table, user)
-            DisplayReport.report_4(display_report)
+            DisplayReport.report_players(display_report)
         else:
             MainView.welcome()
         DisplayReport.menu_report()
 
-    def back_menu(actors_table, tournament_table, user):
+    @classmethod
+    def back_menu(cls, actors_table, tournament_table, user):
         """Display menu to go back to the Main Menu."""
         choice = MainView.back_menu()
-        if choice == 1:
+        choices = {
+            1: "welcome_menu",
+            2: "update_rank",
+            3: "end_program",
+        }
+        if choices[choice] == "welcome_menu":
             MainView.welcome()
-        elif choice == 2:
+        elif choices[choice] == "update_rank":
             Data.update_rank(actors_table, tournament_table, user)
             return MenuController.back_menu()
-        elif choice == 3:
+        elif choices[choice] == "end_program":
             print("Program ended ! See you soon !")
+
         else:
             print("An error occurred.")
             return MenuController.back_menu()
 
-    def inter_menu(actors_table, tournament_table, user):
+    classmethod
+
+    def inter_menu(cls, actors_table, tournament_table, user):
         """Display menu between rounds."""
         choice = MainView.interround_menu()
-        if choice == 1:
+        choices = {
+            1: "continue_tournamemnt",
+            2: "update_rank",
+            3: "welcome_menu",
+            4: "end_program",
+        }
+        if choices[choice] == "continue_tournament":
             pass
-        elif choice == 2:
+        elif choices[choice] == "update_rank":
             Data.update_rank(actors_table, tournament_table, user)
             return MenuController.inter_menu(actors_table, tournament_table, user)
-        elif choice == 3:
+        elif choices[choice] == "welcome_menu":
             MainView.welcome()
-        elif choice == 4:
+        elif choices[choice] == "end_program":
             print("Program ended ! See you soon !")
         else:
             print("An error occurred.")
