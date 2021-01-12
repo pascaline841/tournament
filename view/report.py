f6692f46b3343faa04361a2632aeb5877a040d64
Pascaline M <pascaline.martres@gmail.com>
2021-01-12 12:36:05 -0800

    meilleur presentation

--- view/report.py
+++ view/report.py
@@ -16,6 +16,7 @@ class DisplayReport:
                 raise ValueError
             print("Your choice ({}) has been successfully entered\n".format(report))
             return report
+
         except ValueError:
             print("The value entered doesn't match the possible choices !\n")
             return DisplayReport.menu_report
