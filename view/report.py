577de3a3d367523c3bb027e93d4199bb1286d349
Pascaline M <pascaline.martres@gmail.com>
2021-01-12 18:36:47 -0800

    changement ordre first et last name

--- view/report.py
+++ view/report.py
@@ -7,7 +7,7 @@ class DisplayReport:
         print("************REPORTS**************")
         print("1 - List of all actors")
         print("2 - List of all tournaments")
-        print("3 - List of all rounds or matchs for a tournament")
+        print("3 - List of all rounds for a tournament")
         print("4 - List of all players for a tournament")
         print("5 - return to the general menu")
         try:
@@ -16,7 +16,6 @@ class DisplayReport:
                 raise ValueError
             print("Your choice ({}) has been successfully entered\n".format(report))
             return report
-
         except ValueError:
             print("The value entered doesn't match the possible choices !\n")
             return DisplayReport.menu_report
@@ -25,7 +24,7 @@ class DisplayReport:
     def report_actors(display_report):
         """Display a list of all the actors."""
         for actor in display_report:
-            print(f"{actor['first name']} {actor['last name']} RANK : {actor['rank']}")
+            print(f"{actor['last name']} {actor['first name']} RANK : {actor['rank']}")
 
     @staticmethod
     def report_tournaments(display_report):
