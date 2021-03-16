042097fb86f6e756167e2b68858ef19cc1804b57
Pascaline M <pascaline.martres@gmail.com>
2021-03-15 17:56:58 -0700

    update display reports

--- controllers/report.py
+++ controllers/report.py
@@ -17,38 +17,34 @@ class Reports:
 
     def get_command(self):
         """Display the reports' menu."""
+        db = TinyDB("TOURNAMENTS.json")
         command = Input.for_range(
             "Enter your command (1, 2, 3, 4, 5) : \n", [1, 2, 3, 4, 5]
         )
-        if command == 1:
-            return "sorted_actors report"
-        elif command == 2:
-            return "tournaments report"
-        elif command == 3:
-            return "rounds report"
-        elif command == 4:
-            return "players report"
-        elif command == 5:
-            return "main menu"
-
-    def update(self, command: str):
-        db = TinyDB("TOURNAMENTS.json")
-        if command == "sorted_actors report":
-            display_report = self.sorted_actors()
-            View.report_actors(display_report)
-        elif command == "tournaments report":
+        choices = {
+            1: "sorted users report",
+            2: "tournaments report",
+            3: "rounds report",
+            4: "players report",
+            5: "main menu",
+        }
+        if choices[command] == "sorted users report":
+            display_report = Reports.sorted_users()
+            View.report_users(display_report)
+        elif choices[command] == "tournaments report":
             display_report = db.all()
             View.report_tournaments(display_report)
-        elif command == "rounds report":
+        elif choices[command] == "rounds report":
             display_report = self.request_rounds()
             View.report_rounds(display_report)
-        elif command == "players report":
+        elif choices[command] == "players report":
             display_report = self.request_players()
             View.report_players(display_report)
-        elif command == "main menu":
+        elif choices[command] == "main menu":
             return "main menu"
 
-    def sorted_actors(self):
+    @classmethod
+    def sorted_users(cls):
         """Sort all users by alphabetic order or by rank."""
         db = TinyDB("USERS.json")
         users = db.all()
@@ -58,23 +54,25 @@ class Reports:
         else:
             return sorted(users, key=lambda user: user["rank"])
 
-    def request_players(self):
+    @classmethod
+    def request_players(cls):
         """Request for a tournament to display its players by alpha order or rank."""
         db = TinyDB("TOURNAMENTS.json")
         query = Query()
-        name = Input.for_string("What is it name ? ")
-        command = db.search(query["name"] == name)
-        players = command[0].get("players")
+        name = Input.for_string("What is the tournament's name ? ")
+        data = db.search(query["name"] == name)
+        players = data[0].get("players")
         sorted_choice = Input.for_integer("Sorted by Last Name (1) or by Rank (2) ? ")
         if sorted_choice == 1:
             return sorted(players, key=lambda players: players["last_name"])
         else:
             return sorted(players, key=lambda players: players["rank"])
 
-    def request_rounds(self):
+    @classmethod
+    def request_rounds(cls):
         """Request for a tournament to display its rounds or matchs."""
         db = TinyDB("TOURNAMENTS.json")
         query = Query()
-        name = Input.for_string("What is it name ? ")
-        command = db.search(query["name"] == name)
-        return command[0].get("rounds")
+        name = Input.for_string("What is tournament's name ? ")
+        data = db.search(query["name"] == name)
+        return data[0].get("rounds")
